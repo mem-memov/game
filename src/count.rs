42a713c00e27993019682ccaf3ae2d295566c65b
Alexander Novikov <mem-memov@yandex.ru>
2015-08-01 11:35:28 +0300

    extra indents removed

--- src/count.rs
+++ src/count.rs
@@ -1,37 +1,37 @@
-	#[derive(Debug)]
-	pub struct Count {
-		limit: u32,
-		current: u32
-	}
+#[derive(Debug)]
+pub struct Count {
+	limit: u32,
+	current: u32
+}
 
-	pub trait Counting {
-		fn next(&mut self) -> Option<u32>;
-	}
+pub trait Counting {
+	fn next(&mut self) -> Option<u32>;
+}
 
 
-	impl Count {
-		pub fn new(limit: u32) -> Count {
-			Count{
-				limit: limit,
-				current: 0
-			}
+impl Count {
+	pub fn new(limit: u32) -> Count {
+		Count{
+			limit: limit,
+			current: 0
 		}
 	}
+}
 
-	impl Counting for Count {
-		fn next(&mut self) -> Option<u32> {
-			if self.current == self.limit {
-				None
-			} else {
-				self.current = self.current + 1;
-				Some(self.current)
-			}
+impl Counting for Count {
+	fn next(&mut self) -> Option<u32> {
+		if self.current == self.limit {
+			None
+		} else {
+			self.current = self.current + 1;
+			Some(self.current)
 		}
 	}
+}
 
-	impl <'a> Counting for &'a mut Count {
-		fn next(&mut self) -> Option<u32> {
-			Count::next(*self)
-		}
+impl <'a> Counting for &'a mut Count {
+	fn next(&mut self) -> Option<u32> {
+		Count::next(*self)
 	}
+}
 
